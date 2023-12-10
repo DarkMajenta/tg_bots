25edf626c21e18872c65693650bbf061e399e0cd
DrkMjnt <mackviral@gmail.com>
2023-12-10 20:53:15 +0300

    update

--- parser_tg_on-server.py
+++ parser_tg_on-server.py
@@ -1,64 +1,74 @@
-import telebot
+import json
 import sqlite3
-from google.oauth2 import service_account
+import telebot
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from airtable import Airtable
 
-# Здесь ты можешь настроить свои административные данные
-admin_panel_credentials = 'admin_panel_credentials.json'
-telegram_token = 'токен_телеграм_бота'
-chat_id = 'идентификатор_группы_TG'
-google_credentials = 'google_credentials.json'
-airtable_api_key = 'API_ключ_Airtable'
-airtable_base_key = 'идентификатор_базы_Airtable'
+# Admin Panel Configuration
+with open('admin_panel_credentials.json') as file:
+    admin_panel_credentials = json.load(file)
 
-# Подключение к Google Таблицам
-google_credentials = service_account.Credentials.from_service_account_file(google_credentials)
-google_client = gspread.authorize(google_credentials)
-google_sheet = google_client.open('Название_гугл_таблицы').sheet1
+telegram_token = 'YOUR_TELEGRAM_TOKEN'
+chat_id = 'YOUR_CHAT_ID'
+google_credentials_json = 'google_credentials.json'
+airtable_api_key = 'YOUR_AIRTABLE_API_KEY'
+airtable_base_key = 'YOUR_AIRTABLE_BASE_KEY'
 
-# Подключение к базе данных SQLite
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
+# Google Sheets Integration
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(google_credentials_json, scope)
+client = gspread.authorize(credentials)
+sheet = client.open('Название_гугл_таблицы').sheet1
 
-# Подключение к Airtable
-airtable = Airtable('название_таблицы', 'название_таблицы', api_key=airtable_api_key)
+# SQLite Database Connection
+database = sqlite3.connect('database.db')
+cursor = database.cursor()
 
-# Создаем экземпляр телеграм-бота
-bot = telebot.TeleBot(token=telegram_token)
+# Airtable Integration
+airtable = Airtable('YOUR_AIRTABLE_BASE_ID', 'название_таблицы', api_key='YOUR_AIRTABLE_API_KEY')
 
-# Метод для проверки наличия дубликатов сообщений в базе данных
-def check_duplicate_message(msg):
-    query = "SELECT * FROM messages WHERE message_id = ?"
-    result = cursor.execute(query, (msg.message_id,))
-    return result.fetchone() is not None
+# Saving and Checking Duplicate Messages
+def check_duplicate_message(message_id):
+    cursor.execute(f"SELECT * FROM messages WHERE message_id = {message_id}")
+    result = cursor.fetchone()
+    return result is not None
 
-# Метод для сохранения сообщения в базе данных
-def save_message(msg):
-    query = "INSERT INTO messages (message_id, chat_id, text) VALUES (?, ?, ?)"
-    cursor.execute(query, (msg.message_id, msg.chat.id, msg.text))
-    conn.commit()
+def save_message(message_id, chat_id, text):
+    cursor.execute(f"INSERT INTO messages (message_id, chat_id, text) VALUES ({message_id}, {chat_id}, '{text}')")
+    database.commit()
 
-# Метод для пересылки сообщений
-def forward_message(msg):
-    bot.forward_message(chat_id, msg.chat.id, msg.message_id)
+# Forwarding Messages
+def forward_message(message):
+    bot.forward_message(chat_id, message.chat.id, message.message_id)
 
-    # Дополнительные действия:
-    # Пересылка в гугл таблицу
-    google_sheet.append_row([msg.text, msg.chat.id])
+    # Append message text and chat ID to the Google Sheet
+    sheet.append_row([message.text, message.chat.id])
 
-    # Пересылка в базу данных Airtable
-    airtable.insert({'text': msg.text, 'chat_id': msg.chat.id})
+    # Insert message text and chat ID to Airtable
+    airtable.insert({'Text': message.text, 'Chat ID': message.chat.id})
 
-@bot.message_handler(func=lambda msg: True)
-def handle_message(msg):
-    if not check_duplicate_message(msg):
-        # Проверка на наличие ключевых слов
-        keywords = ['ключевое_слово_1', 'ключевое_слово_2']
-        minus_keywords = ['минус_слово_1', 'минус_слово_2']
+# Message Handling
+def handle_message(message):
+    if message.text:
+        message_id = message.message_id
+        chat_id = message.chat.id
+        text = message.text
 
-        if any(keyword in msg.text for keyword in keywords) and not any(minus_keyword in msg.text for minus_keyword in minus_keywords):
-            forward_message(msg)
-            save_message(msg)
+        if not check_duplicate_message(message_id):
+            keywords = ['keyword1', 'keyword2']  # Add your keywords here
+            minus_keywords = ['minus', 'keyword']  # Add your minus keywords here
+
+            if any(keyword in text for keyword in keywords) and not any(keyword in text for keyword in minus_keywords):
+                forward_message(message)
+                save_message(message_id, chat_id, text)
+
+# Start the Bot
+bot = telebot.TeleBot(telegram_token)
+
+@bot.message_handler(func=lambda message: True)
+def handle_all_messages(message):
+    handle_message(message)
 
 bot.polling(none_stop=True)
